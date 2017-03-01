294e42f23d325f37d29f02228284308ad7abb49c
LengQue1 <lengque6@gmail.com>
2017-03-01 17:47:40 +0800

    new pageing style button

--- client/front/src/components/pagination.vue
+++ client/front/src/components/pagination.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="pagination container">
-        <router-link v-if="page > 1" :to="{ query: { page: parseInt(page) - 1} }" class="prev">&laquo; 上一页</router-link>
-        <router-link v-if="page < totalPage" :to="{ query: { page: parseInt(page) + 1}  }" class="next">下一页 &raquo;</router-link>
+        <router-link v-if="page > 1" :to="{ query: { page: parseInt(page) - 1} }" class="prev">上一页</router-link>
+        <router-link v-if="page < totalPage" :to="{ query: { page: parseInt(page) + 1}  }" class="next">下一页</router-link>
     </nav>
 </template>
 
@@ -18,6 +18,19 @@
 </script>
 
 <style>
+    .pagination a {
+        display: inline-block;
+        margin: 0 4px;
+        padding: 4px 12px;
+        border-radius: 4px;
+        border-bottom: 4px solid #3aa373;
+        font-size: 12px;
+        color: #fff;
+        background-color: #4fc08d;
+    }
+    .pagination a:hover {
+        background-color: #22bd77;
+    }
     .pagination {
         justify-content: space-around;
     }
